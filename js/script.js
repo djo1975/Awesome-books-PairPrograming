d67a44285ebc1c49d7e6b5a39df479127f75398e
Mladan Ilic <116818129+djo1975@users.noreply.github.com>
2023-01-17 08:57:11 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,53 +1,67 @@
-let bookCollection = [];
-/* eslint-disable no-unused-vars */
+/* eslint-disable max-classes-per-file, arrow-parens */
 
-// load book collection from local storage
-if (localStorage.getItem('bookCollection')) {
-  bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
-}
-function displayBooks() {
-  const bookList = document.getElementById('book-list');
-  bookList.innerHTML = '';
-  bookCollection.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-    bookDiv.id = `book-${book.id}`;
-    bookDiv.innerHTML = `
-      <h2 class="author-name">${book.author}</h2>
-      <h3 class="book-title">${book.title}</h3>
-      <button type="submit" id="remove-button" onClick="removeBookFromCollection(${book.id})">Remove</button>
-      <hr>
-    `;
-    bookList.appendChild(bookDiv);
-  });
-}
-displayBooks();
-function addBookToCollection(title, author) {
-  const newBook = {
-    id: Date.now(),
-    title,
-    author,
-  };
-  bookCollection.push(newBook);
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-  displayBooks();
+class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
 
-/**
- * Removes a book from the collection.
- * Called when the 'Remove' button is clicked
- */
-function removeBookFromCollection(id) {
-  bookCollection = bookCollection.filter((book) => book.id !== id);
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-  displayBooks();
+class BookCollection {
+  constructor() {
+    this.collection = [];
+    this.load();
+  }
+
+  load() {
+    if (localStorage.getItem('bookCollection')) {
+      this.collection = JSON.parse(localStorage.getItem('bookCollection')).map((book) => new Book(book.title, book.author, book.id));
+    }
+  }
+
+  add(title, author) {
+    const newBook = new Book(title, author, Date.now());
+    this.collection.push(newBook);
+    localStorage.setItem('bookCollection', JSON.stringify(this.collection));
+  }
+
+  remove(id) {
+    this.collection = this.collection.filter((book) => book.id !== id);
+    localStorage.setItem('bookCollection', JSON.stringify(this.collection));
+  }
+
+  display() {
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+    this.collection.forEach((book) => {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book');
+      bookDiv.id = `book-${book.id}`;
+      bookDiv.innerHTML = `
+            <h2 class="author-name">"${book.title}"</h2>
+            <h3 class="book-title">by- ${book.author}</h3>
+            <button type="submit" id="remove-button">Remove</button>
+        `;
+      const removeButton = bookDiv.querySelector('#remove-button');
+      removeButton.addEventListener('click', () => {
+        this.remove(book.id);
+        bookDiv.remove();
+      });
+      bookList.appendChild(bookDiv);
+    });
+  }
 }
+const bookCollection = new BookCollection();
+bookCollection.display();
+
 const addBookForm = document.getElementById('add-book-form');
 addBookForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const bookTitle = document.getElementById('book-title').value;
   const authorName = document.getElementById('author-name').value;
-  addBookToCollection(bookTitle, authorName);
+  bookCollection.add(bookTitle, authorName);
   document.getElementById('book-title').value = '';
   document.getElementById('author-name').value = '';
+  bookCollection.display();
 });
